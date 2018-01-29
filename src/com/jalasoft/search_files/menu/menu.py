52afc0de24293e65c66bc6644b016006176e5caa
bqeanzol <bqeanzol@us.ibm.com>
2018-01-29 13:54:00 -0400

    Not OO menu implemented until the first option

--- src/com/jalasoft/search_files/menu/menu.py
+++ src/com/jalasoft/search_files/menu/menu.py
@@ -15,12 +15,12 @@ menu_actions = {}
 # Main menu
 def main_menu():
     os.system("cls")
-
     print("Welcome,\n")
-    print("Please choose the menu you want to start:")
-    print("1. Search Path")
-    print("2. Search File")
-    print("\n0. Quit")
+    print("Please choose the option you want to start:")
+    print("1. Search all assets from a path")
+    print("2. Search all files in a path")
+    print("3. Search all directories in a path")
+    print("0. Quit")
     choice = input(" >>  ")
     exec_menu(choice)
 
@@ -34,9 +34,6 @@ def exec_menu(choice):
     if ch == '':
         menu_actions['main_menu']()
     else:
-        if is_number(ch) and is_in_range(ch, ****):
-            if ()
-
         try:
             menu_actions[ch]()
         except KeyError:
@@ -45,27 +42,68 @@ def exec_menu(choice):
             menu_actions['main_menu']()
     return
 
+# Execute menu
+def exec_menu1(choice):
+    os.system("cls")
+    ch = choice.lower()
+    if ch == '':
+        menu_actions['main_menu']()
+    else:
+        try:
+            menu1_actions[ch]()
+        except KeyError:
+            print
+            "Invalid selection, please try again.\n"
+            menu_actions['main_menu']()
+    return
+
 # Menu 1
 def menu1():
-    print("1. Find all files form a path \n")
-    print("2. Find all dirs from a path \n")
-    print("3. Find all file and folders from a path")
+    _ = os.system("cls")
+    print("Searching for assets \n")
+    print("1. Find all assets")
+    print("2. Find assets by name")
+    print("2. Find assets by size")
     print("9. Back")
     print("0. Quit")
     choice = input(" >>  ")
-    exec_menu(choice)
+    exec_menu1(choice)
     return
 
 # Menu 2
 def menu2():
-    print("File foundd"
-          " !\n")
+    os.system("cls")
+    print("Searching for files \n")
+    print("1. Find files by extension")
+    print("2. Find files by name")
+    print("3. Find files by size")
+    print("9. Back")
+    print("0. Quit")
+    choice = input(">>  ")
+    exec_menu(choice)
+    return
+
+# Menu 3
+def menu3():
+    os.system("cls")
+    print ("Sarching for folders \n")
+    print("1. Find folders by name")
+    print("2. Find folders by size")
     print("9. Back")
     print("0. Quit")
     choice = input(" >>  ")
     exec_menu(choice)
     return
 
+def find_all_assets():
+    path = input(" Enter the path >>  ")
+    print("Find all assets was selected. Starting the searching process in", path)
+
+def find_assets_by_name():
+    print("Find all assets by name was selected. Starting the searching process ...")
+
+def find_assets_by_size ():
+    pass
 
 # Back to main menu
 def back():
@@ -86,6 +124,15 @@ menu_actions = {
     'main_menu': main_menu,
     '1': menu1,
     '2': menu2,
+    '3': menu3,
+    '9': back,
+    '0': exit,
+}
+
+menu1_actions = {
+    '1': find_all_assets,
+    '2': find_assets_by_name,
+    '3': find_assets_by_size,
     '9': back,
     '0': exit,
 }
