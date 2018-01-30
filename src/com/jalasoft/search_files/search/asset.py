a56486d4b68cb2ca7f4e6a490cb73c6245e508e2
Marcela Barrionuevo <marcela.barrionuevo@jalasoft.com>
2018-01-30 04:20:18 -0400

    commit with first iteration changes
    commit with first iteration changes

--- src/com/jalasoft/search_files/search/asset.py
+++ src/com/jalasoft/search_files/search/asset.py
@@ -1,27 +1,31 @@
+"""
+Asset class
+"""
+import os
 class Asset(object):
-    def __init__(self, path, name, size):
+    def __init__(self, path, name):
+        """
+
+        :param path: path entered for the user to any search
+        :param name:this contains file or directory name
+        """
+
         self.path = path
         self.name = name
-        self.size = size
+        self.size = os.path.getsize(path)
         self.is_directory = False
 
     def get_path(self):
         return self.path
 
-    def get_name(self):
-        return self.name
-
     def get_size(self):
         return self.size
 
-    def set_path(self, path):
-        self.path = path
-
-    def set_name(self, name):
-        self.name = name
-
-    def set_size(self, size):
-        self.size = size
-
     def get_is_directory(self):
-        return self.is_directory
+        return True
+
+    def get_name(self):
+        return self.name
+
+    def set_is_directory(self, asset):
+        self.is_directory = asset
--- src/com/jalasoft/search_files/search/search.py
+++ src/com/jalasoft/search_files/search/search.py
@@ -1,35 +1,86 @@
+"""
+class Search perform the search on a given path
+working according custom filters
+"""
 from  src.com.jalasoft.search_files.search.directory import Directory
 from  src.com.jalasoft.search_files.search.file import  File
 import os
+import fnmatch
+from src.com.jalasoft.search_files.utils.logging_config import LOGGER as LOGGER
 
 
-class Search(object):
+
+class Search():
     def __init__(self, base_path):
+        """
+
+        :param base_path: this parameter is main to search by any criteria
+        """
         self.base_path = base_path
-        self.directory = Directory()
-        self.file = File("", "", "", "")
 
 
-    def search_all_directories(self):
-        list_asset = []
+    def search_files_and_directories(self):
+        """
+
+        :return:
+        """
+
+        LOGGER.info("=========start setting new value for search criteria=========")
+        result = []
         for root, directories, files in os.walk(self.base_path):
-            for dirs in directories:
-                dir_all = os.path.join(root, dirs)
-                list_asset.append(dir_all)
-            for file in files:
-                file = os.path.join(root, file)
-                list_asset.append(file)
-        return list_asset
+            for dir in directories:
+                directory = Directory(os.path.join(root, dir), dir)
+                directory.set_is_directory(True)
+                result.append(os.path.join(self.base_path,directory.get_name()))
+                LOGGER.debug(self,'directories search', directory)
 
+            for file in files:
+                file = File(os.path.join(root, file), file, "")
+                file.set_is_directory(False)
+                result.append(os.path.join(root,file.get_name()))
+                LOGGER.info('Doing something')
+        return result
 
     def search_all_files(self):
         pass
-
     def search_by_filter(self):
         pass
 
-    def search_file_by_extesion(self):
-        pass
+    def search_file_by_extension(self, extension):
+        """
+
+        :param extension:
+        :return:
+        """
+
+        result = []
+        for root, directories, files in os.walk(self.base_path):
+            for file in files:
+                file = File(os.path.join(root, file), file, extension)
+                file.set_is_directory(False)
+                if file.get_name().endswith(file.get_extension()):
+                    result.append(os.path.join(root, file.get_name()))
+
+        return result
+
+    def searh_by_name(self, name):
+        """
+
+        :param name:
+        :return:
+        """
+        result = []
+        for root, directories, files in os.walk(self.base_path):
+            for file in files:
+                file = File(os.path.join(root, file), name, "")
+                file.set_is_directory(False)
+        for file in os.listdir(self.base_path):
+            if fnmatch.fnmatch(file, name):
+                result.append(file)
+        return result
+
+
+
 
     def search_file_by_content(self):
         pass
@@ -51,4 +102,7 @@ class Search(object):
 
 if __name__ == "__main__":
     search = Search("C:\\test")
-    print(search.search_all_directories())
+
+    print(search.search_files_and_directories())
+    print(search.search_file_by_extension(".txt"))
+    print(search.searh_by_name("kate.*"))
