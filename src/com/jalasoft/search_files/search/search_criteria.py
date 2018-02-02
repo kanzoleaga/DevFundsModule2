51631fd01f49ae5b60af0e1091a8ff926ba74f65
bqeanzol <bqeanzol@us.ibm.com>
2018-02-02 05:34:16 -0400

    Search criteria first draft

--- src/com/jalasoft/search_files/search/search_criteria.py
+++ src/com/jalasoft/search_files/search/search_criteria.py
@@ -1,7 +1,31 @@
 class SearchCriteria(object):
-    pass
+    def __init__(self, criteria ):
+        """
+        This is the contructor of the SearchCriteria class.
+        :param criteria: dic  the dictionary with keys as criterias to search
+        The form of the criteria dictionaly is as follows:
+        Where size_range is a tuple (from_size, to_size)
+        Where create_date_range is a tuple (from_date, to_date)
+        Where asset_type is 'file' or 'dir'
+        """
+        self.criteria = {
+                        'path': criteria['path'],
+                        'extension': criteria['extension'],
+                        'name': criteria['name'],
+                        'size_range': (criteria['size_range']),
+                        'owner': criteria['owner'],
+                        'create_date_range': criteria['create_date_range'],
+                        'asset_type': criteria['asset_type']
+        }
 
-class BasicSearch(SearchCriteria):
-    pass
+    def get_criteria_value(self, key):
+        if key in self.criteria.keys:
+            return self.criteria[key]
+        else:
+            raise ValueError('Invalid key. Key value' + key + 'is not a valid criteria')
 
-class
+
+criteria = {'path': 'c:\test', 
+            'extension': '.txt'}
+search_criteria = SearchCriteria(criteria)
+print (extension = search_criteria.get_criteria_value('extension'))
