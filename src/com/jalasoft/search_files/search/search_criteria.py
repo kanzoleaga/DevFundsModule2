32caf62d765543ac1f506974a995ae7c9eacaad1
bqeanzol <bqeanzol@us.ibm.com>
2018-02-02 20:25:11 -0400

    Search criteria wit positional arguments

--- src/com/jalasoft/search_files/search/search_criteria.py
+++ src/com/jalasoft/search_files/search/search_criteria.py
@@ -1,5 +1,6 @@
 class SearchCriteria(object):
-    def __init__(self, criteria ):
+    def __init__(self, path, name, extension, asset_type=None, size_range=None, size_unit=None, owner=None,
+                 create_date_range=None):
         """
         This is the contructor of the SearchCriteria class.
         :param criteria: dic  the dictionary with keys as criterias to search
@@ -9,23 +10,25 @@ class SearchCriteria(object):
         Where asset_type is 'file' or 'dir'
         """
         self.criteria = {
-                        'path': criteria['path'],
-                        'extension': criteria['extension'],
-                        'name': criteria['name'],
-                        'size_range': (criteria['size_range']),
-                        'owner': criteria['owner'],
-                        'create_date_range': criteria['create_date_range'],
-                        'asset_type': criteria['asset_type']
+                        'path': path,
+                        'extension': extension,
+                        'name': name,
+                        'size_range': size_range,
+                        'size_unit': size_unit,
+                        'owner': owner,
+                        'create_date_range': create_date_range,
+                        'asset_type': asset_type
         }
 
     def get_criteria_value(self, key):
-        if key in self.criteria.keys:
+        if key in self.criteria.keys():
             return self.criteria[key]
         else:
             raise ValueError('Invalid key. Key value' + key + 'is not a valid criteria')
 
 
-criteria = {'path': 'c:\test', 
+criteria = {'path': 'c:\test',
+            'name': 'test',
             'extension': '.txt'}
-search_criteria = SearchCriteria(criteria)
-print (extension = search_criteria.get_criteria_value('extension'))
+search_criteria = SearchCriteria('c:\test','test', '.txt')
+print (search_criteria.get_criteria_value('extension'))
