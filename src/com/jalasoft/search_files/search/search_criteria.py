7bf33adcbd2c43c914a3a11a16790f94eeb675a6
bqeanzol <bqeanzol@us.ibm.com>
2018-02-02 20:40:02 -0400

    Comments to Search Criteria

--- src/com/jalasoft/search_files/search/search_criteria.py
+++ src/com/jalasoft/search_files/search/search_criteria.py
@@ -1,13 +1,21 @@
 class SearchCriteria(object):
     def __init__(self, path, name, extension, asset_type=None, size_range=None, size_unit=None, owner=None,
                  create_date_range=None):
+
         """
-        This is the contructor of the SearchCriteria class.
-        :param criteria: dic  the dictionary with keys as criterias to search
-        The form of the criteria dictionaly is as follows:
+        This is the constructor of the SearchCriteria class.
         Where size_range is a tuple (from_size, to_size)
         Where create_date_range is a tuple (from_date, to_date)
         Where asset_type is 'file' or 'dir'
+        Where size_unit is either 'B', 'KB', 'MB', 'GB'
+        :param path:
+        :param name:
+        :param extension:
+        :param asset_type:
+        :param size_range:
+        :param size_unit:
+        :param owner:
+        :param create_date_range:
         """
         self.criteria = {
                         'path': path,
@@ -21,6 +29,12 @@ class SearchCriteria(object):
         }
 
     def get_criteria_value(self, key):
+        """
+        This returns the value of an specific search criteria. For example it will return '.txt'
+        in case the extension criteria has this value
+        :param key: str the key name of the criteria
+        :return:  the value of the criteria it may be an string or an integer
+        """
         if key in self.criteria.keys():
             return self.criteria[key]
         else:
