6d79c310e098bb49ac890950f9b4b6cfa783687d
Marcela Barrionuevo <marcela.barrionuevo@jalasoft.com>
2018-02-18 22:07:43 -0400

    fixed serach in c:\
    fixed serach in c:\

--- src/com/jalasoft/search_files/search/file.py
+++ src/com/jalasoft/search_files/search/file.py
@@ -10,10 +10,7 @@ class File(Asset):
     def __init__(self, path, name):
         super().__init__(path, name)
         self.extension = os.path.splitext(name)[1]
-        # Get file owner
-        file_and_folder = win32security.GetFileSecurity(self.path, win32security.OWNER_SECURITY_INFORMATION)
-        file_owner = win32security.LookupAccountSid(None, file_and_folder.GetSecurityDescriptorOwner())
-        self.owner = file_owner[0]
+        self.owner = ''
 
     def get_extension(self):
         return self.extension
@@ -21,7 +18,18 @@ class File(Asset):
     def get_owner(self):
         return self.owner
 
+    def set_owner(self):
+        sec_info = win32security.GetFileSecurity(self.path, win32security.OWNER_SECURITY_INFORMATION)
+        sid = sec_info.GetSecurityDescriptorOwner()
+        self.owner = win32security.LookupAccountSid(None, sid)[0]
+
+
+
+
     # Size was not set as part of the constructor because of efficiency, It will be called only
     # if the search criteria includes size
     def set_size(self):
         self.size = os.path.getsize(self.path)
+
+
+
